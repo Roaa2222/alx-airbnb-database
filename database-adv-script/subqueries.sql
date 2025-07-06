1ba94445468753ee0d820ccd155e3542c36e6714
Roaa2222 <133399768+Roaa2222@users.noreply.github.com>
2025-07-06 07:19:18 +0300

    Create subqueries.sql

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,23 @@
+-- 🔹 Non-Correlated Subquery:
+-- Find all properties with an average rating greater than 4.0
+
+SELECT property_id, name, location
+FROM properties
+WHERE property_id IN (
+    SELECT property_id
+    FROM reviews
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
+
+
+-- 🔹 Correlated Subquery:
+-- Find users who have made more than 3 bookings
+
+SELECT user_id, first_name, last_name, email
+FROM users u
+WHERE (
+    SELECT COUNT(*)
+    FROM bookings b
+    WHERE b.user_id = u.user_id
+) > 3;

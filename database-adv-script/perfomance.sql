6765e0eb96669c1666db2046cbcef8684ac5c408
Roaa2222 <133399768+Roaa2222@users.noreply.github.com>
2025-07-06 08:30:30 +0300

    Create perfomance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,21 @@
+-- 🔹 Initial Complex Query
+-- Retrieve all bookings with user, property, and payment details
+
+SELECT
+  b.booking_id,
+  b.start_date,
+  b.end_date,
+  b.total_price,
+  u.user_id,
+  u.first_name,
+  u.last_name,
+  p.property_id,
+  p.name AS property_name,
+  pay.payment_id,
+  pay.amount,
+  pay.payment_method
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id
+ORDER BY b.start_date DESC;
